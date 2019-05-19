ddea15e775338ce0c0e5b4f6f50793611a6914fa
Paweł Bajda <pawel.bajda@gmail.com>
2019-05-19 11:18:11 +0200

    push pop i testy

--- src/main/java/day2/QueueSDA.java
+++ src/main/java/day2/QueueSDA.java
@@ -2,13 +2,58 @@ package day2;
 
 public class QueueSDA {
 
-    int capacity;
+    private final int capacity;
+    private final String[] kolejka;
+    private int tail;
+    private int head;
+    private int size;
 
     public QueueSDA(int capacity) {
+        this.kolejka = new String[capacity];
         this.capacity = capacity;
+        this.size = 0;
+        this.head = 0;
+        this.tail = 0;
     }
 
     public int getMaxSize() {
-    return capacity;
+        return capacity;
+    }
+
+
+    public void push(String x) {
+
+        if(isFull()){
+            throw new QueueIsFullException();
+        }
+
+        this.kolejka[this.head] = x;
+        this.head++;
+        this.size++;
+    }
+
+    public boolean isFull() {
+        return this.capacity == this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String pop() {
+
+        if (isEmpty()) {
+            throw new NoItemsInQueueException();
+        }
+        String returnedObject = this.kolejka[this.tail];
+        this.kolejka[this.tail] = null;
+        this.tail++;
+        this.size--;
+        return returnedObject;
     }
 }
+
--- src/test/java/day2/QueueSDATest.java
+++ src/test/java/day2/QueueSDATest.java
@@ -10,4 +10,31 @@ public class QueueSDATest {
         QueueSDA queue = new QueueSDA(10);
         Assert.assertEquals(queue.getMaxSize(), 10);
     }
+    @Test
+    public void test2_addItemToFifo(){
+        QueueSDA queue = new QueueSDA(10);
+        queue.push("pierwszy element");
+        queue.push("drugi element");
+
+        Assert.assertEquals(queue.getSize(), 2);
+
+        Assert.assertEquals(queue.pop(), "pierwszy element");
+        Assert.assertEquals(queue.pop(), "drugi element");
+    }
+
+    @Test (expected = NoItemsInQueueException.class)
+    public void test3_isEmptyException(){
+        QueueSDA queue = new QueueSDA(10);
+        queue.pop();
+
+    }
+    @Test(expected = QueueIsFullException.class)
+    public void test4_isFullException(){
+        QueueSDA queue = new QueueSDA(3);
+        queue.push("a");
+        queue.push("b");
+        queue.push("c");
+        queue.push("d");
+
+    }
 }
